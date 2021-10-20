ce5c1a38547b70a6195733033db201069a390568
Your Name <you@example.com>
2021-10-20 18:56:43 +0300

    add express server

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const routes = require('./routes');
+const bodyParser = require('body-parser');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+app.use('/api', routes);
+
+app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
