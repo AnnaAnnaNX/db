8186716a57ca87d69309ff4513375f41de8bd50e
Your Name <you@example.com>
2021-10-22 14:06:06 +0300

    read excel data from file

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,17 @@
 const { Product } = require('../models');
+const { getInfoFromFile } = require('../utils/helpers');
+
 const createProduct = async (req, res) => {
     try {
         console.log(req.file);
 
+        // get type load file - 'ym', 'ozon', 'products', 'Ост_база'
+
+
+        // get product info while type file
+        const content = await getInfoFromFile('ym', req.file);
+
+        // bulk
         const product = await Product.create(req.body);
         return res.status(201).json({
             product,
