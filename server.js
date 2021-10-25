5811302dc47361ee0cc2cbde34b9e82e4c57fb7e
AnnAnnaNX <anna.annanx@rambler.ru>
2021-10-25 13:04:13 +0300

    access index page on public folder

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const routes = require('./routes');
 const bodyParser = require('body-parser');
+const path = require('path');
+var public = path.join(__dirname, 'public');
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use('/api', routes);
+
+app.get('/', function(req, res) {
+    res.sendFile(path.join(public, 'index.html'));
+});
 
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
